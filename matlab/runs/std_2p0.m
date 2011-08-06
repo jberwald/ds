8256e956f4e76e37670f658b0c6afe1dc30ffde2
Raf <rafael.frongillo@gmail.com>
2011-08-06 13:35:37 -0700

    updated a few std map routines

--- matlab/runs/std_2p0.m
+++ matlab/runs/std_2p0.m
@@ -1,33 +1,49 @@
+
+tstart = tic;
 map = get_map('std',6,'params',2);
 [map d8] = homoclinic_driver(map,8);
 save_map_special(map,'std_2p0d8');
+toc(tstart);
 
+tstart = tic;
 map = get_map('std',6,'params',2);
 [map d9] = homoclinic_driver(map,9);
 save_map_special(map,'std_2p0d9');
+toc(tstart);
 
+tstart = tic;
 map = get_map('std',6,'params',2);
 [map d10] = homoclinic_driver(map,10);
 save_map_special(map,'std_2p0d10');
+toc(tstart);
 
+tstart = tic;
 map = get_map('std',6,'params',2);
 [map d11] = homoclinic_driver(map,11);
 save_map_special(map,'std_2p0d11');
+toc(tstart);
 
+tstart = tic;
 map = get_map('std',6,'params',2);
 [map d12] = homoclinic_driver(map,12);
 save_map_special(map,'std_2p0d12');
+toc(tstart);
 
+tstart = tic;
 map = get_map('std',6,'params',2);
 [map d13] = homoclinic_driver(map,13);
 save_map_special(map,'std_2p0d13');
+toc(tstart);
 
+tstart = tic;
 map = get_map('std',6,'params',2);
 [map d14] = homoclinic_driver(map,14);
 save_map_special(map,'std_2p0d14');
+toc(tstart);
 
 %map = get_map('std',6,'params',2);
 %[map d15] = homoclinic_driver(map,15);
 %save_map_special(map,'std_2p0d15');
+%toc(tstart);
 
 save std_2p0.mat d*
--- matlab/std/std_run_int.m
+++ matlab/std/std_run_int.m
@@ -1,8 +1,11 @@
 function std_run_int(eps,width,depth,opts)
+  tstart=tic;
   params = infsup(eps,eps+width);
   fname = strrep(sprintf('std_%1.3f_d%i_%03.0f',eps,depth,1000*width),'.','p')
   map = get_map('std',6,'params',params);
   [map dat] = homoclinic_driver(map,depth,opts);
   save_map_special(map,sprintf('%s_map',fname));
   save(['output/' fname '.mat'],'dat')
+  fprintf('total time -- ')
+  toc(tstart)
 end
