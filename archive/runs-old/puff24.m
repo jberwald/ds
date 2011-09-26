8a156e677dac8fdc2283de8031a51b78cbcf1b94
Raf <rafael.frongillo@gmail.com>
2011-09-26 15:08:25 -0700

    random changes (?)

--- archive/runs-old/puff24.m
+++ archive/runs-old/puff24.m
@@ -1,2 +1,2 @@
-x[R G M SM X A] = puff_driver('henon', 24, 7, 'box', [1.425 0.425]);
+[R G M SM X A] = puff_driver('henon', 24, 7, 'box', [1.425 0.425]);
 save results_puff24_old_dijk.mat
--- matlab/plateaus/utilities/plats_num2depth.m
+++ matlab/plateaus/utilities/plats_num2depth.m
@@ -3,4 +3,4 @@ function [depth box d] = plats_num2depth(num,depths,dlen)
 % compute the depth and box corresponding to data matrix position 'num'
   depth = depths(ceil(num / dlen));
   d = mod(num-1,dlen)+1;
-  box = (2 - d/dlen)*[2 2];
+  box = (2 - d/dlen)*[1 1];
